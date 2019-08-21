045f95c070c557186af80cee8f24c0c384694818
Steven Bui <buisteven.collide@gmail.com>
2019-08-21 15:37:26 -0700

    Home complete

---
+++ src/components/Home/artistsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const ArtistsList = (props) =>{
+  return(
+    <div className="artists_list">
+      <h4>Browse the Artist</h4>
+    </div>
+  )
+}
+
+export default ArtistsList;
---
+++ src/components/Home/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import axios from 'axios';
+import Header from '../header';
+import Banner from './banner';
+import ArtistsList from './artistsList'
+
+const URL_ARTIST = 'http://localhost:3004/artists';
+
+class Home extends React.Component{
+
+  state = {
+    artists:''
+  }
+
+  componentDidMount(){
+    axios.get(URL_ARTIST)
+    .then((response)=>{this.setState({artists:response.data})
+    })
+
+  }
+
+  render(){
+    console.log(this.state);
+    return(
+      <>
+        <Header/>
+        <Banner/>
+        <ArtistsList allArtists = {this.state.artists}/>
+        <div>Home Class</div>
+      </>
+    )
+  }
+}
+
+
+
+
+
+
+export default Home;
